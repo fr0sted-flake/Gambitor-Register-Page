5bcb8ac7710bf8e2d3c8cf92822a06f7a3a8a797
Ayush Bhardwaj <ayush@lifeguru.app>
2024-03-20 16:41:56 +0530

    initialised firebase

--- script.js
+++ script.js
@@ -1,38 +1,82 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAoV-ZoNDu1KNAeq6if61uZcEVWfdDckT0",
-    authDomain: "gambitor-login.firebaseapp.com",
-    projectId: "gambitor-login",
-    storageBucket: "gambitor-login.appspot.com",
-    messagingSenderId: "1021237623050",
-    appId: "1:1021237623050:web:04ed8b7f00fe0c1d58a2f3",
-    measurementId: "G-8T2W9HDDCN"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAoV-ZoNDu1KNAeq6if61uZcEVWfdDckT0",
+  authDomain: "gambitor-login.firebaseapp.com",
+  projectId: "gambitor-login",
+  storageBucket: "gambitor-login.appspot.com",
+  messagingSenderId: "1021237623050",
+  appId: "1:1021237623050:web:04ed8b7f00fe0c1d58a2f3",
+  measurementId: "G-8T2W9HDDCN",
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
 
+console.log(app)
 
+//Scrolling Listener
+window.addEventListener("scroll", function () {
+  var scrollPosition = window.scrollY;
+  var scrollingDiv = document.getElementById("navbar");
 
+  if (scrollPosition > 40) {
+    scrollingDiv.style.backgroundColor = "#150E4E";
+  } else {
+    scrollingDiv.style.backgroundColor = "rgba(255, 255, 255, 0)";
+  }
+});
 
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var scrollingDiv = document.getElementById('navbar');
-  
-    
-    if (scrollPosition > 40) {
-      scrollingDiv.style.backgroundColor = "#150E4E"; 
-    } else {
-      scrollingDiv.style.backgroundColor = 'rgba(255, 255, 255, 0)'; 
-    }
-  });
-  
+//SIGN UP
 
+const signUp = document.getElementById("sign-up");
+
+signUp.addEventListener("click", function (event) {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      alert("User signed up successfully!");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("Error: " + errorMessage);
+      // ..
+    });
+});
+
+//SIGN IN
+
+const signIn = document.getElementById("sign-in");
+
+signIn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      alert("User signed in successfully!");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("Error: " + errorMessage);
+    });
+});
